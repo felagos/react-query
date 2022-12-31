01450d5f86a531095c5ae8875ecf1da78bd1b3f4
Felipe Lagos <felagossanchez@gmail.com>
2022-12-31 19:49:53 -0300

    class to background selected label

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -18,7 +18,7 @@ export const LabelPicker = ({ selectedLabels, onChange }: Props) => {
 				labels?.map((label) => (
 					<span
 						key={label.id}
-						className={`badge rounded-pill m-1 label-picker ${selectedLabels.includes(label.name) ? 'label-active' : ''} `}
+						className={`badge rounded-pill m-1 label-picker ${selectedLabels.includes(label.name) ? 'label-active' : ''}`}
 						style={{ border: `1px solid #${label.color}`, color: `#${label.color}` }}
 						onClick={onChange(label.name)}
 					>
