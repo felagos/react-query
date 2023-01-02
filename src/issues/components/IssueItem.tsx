e9912c238926e8d9ba64cfcb312d3e80121af9b2
Felipe Lagos <felagossanchez@gmail.com>
2023-01-02 16:57:32 -0300

    set and prefetch data

--- src/issues/components/IssueItem.tsx
+++ src/issues/components/IssueItem.tsx
@@ -1,6 +1,8 @@
+import { useQueryClient } from '@tanstack/react-query';
 import { FiInfo, FiCheckCircle } from 'react-icons/fi';
 import { useNavigate } from 'react-router-dom';
 import { Issue, State } from '../../models';
+import { fetchIssueById, fetchIssueComments } from '../../services/git.service';
 import { Avatar } from '../../shared';
 
 interface Props {
@@ -9,11 +11,38 @@ interface Props {
 
 export const IssueItem = ({ issue }: Props) => {
 	const navigate = useNavigate();
+	const queryClient = useQueryClient();
 
 	const doNavigation = () => navigate(`/issues/issue/${issue.number}`)
 
+	const prefetchData = () => {
+		queryClient.prefetchQuery(
+			['issue', issue.number],
+			() => fetchIssueById(issue.number)
+		)
+
+		queryClient.prefetchQuery(
+			['issue', issue.number, 'comments'],
+			() => fetchIssueComments(issue.number)
+		)
+	}
+
+	const presetData = () => {
+		queryClient.setQueryData(
+			['issue', issue.number],
+			issue,
+			{
+				updatedAt: new Date().getTime() + 100000
+			}
+		)
+	}
+
 	return (
-		<div className="card mb-2 issue" onClick={doNavigation}>
+		<div className="card mb-2 issue"
+			onClick={doNavigation}
+			// onMouseEnter={prefetchData}
+			onMouseEnter={presetData}
+		>
 			<div className="card-body d-flex align-items-center">
 
 				{issue.state === State.Open
